b371fb16e03c5e12a1e05ec0df95226b842651a7
wons0 <114294375+wons0@users.noreply.github.com>
2025-01-17 19:12:33 +0100

    Added polish translations

--- src_birthcake/birthcake_translations.lua
+++ src_birthcake/birthcake_translations.lua
@@ -45,6 +45,7 @@ local CAKE ={
 	ru = "Пироженое",
 	spa = "Pastel",
 	cs_cz = "Dort",
+	pl = "Babeczka",
 }
 --is a LIE
 --ТОРТ ЭТО ЛОЖЬ
@@ -58,60 +59,70 @@ local translations = {
 			ru = CAKE.ru .. " Исаака",
 			spa = CAKE.spa .. " de Isaac",
 			cs_cz = "Izákův " .. CAKE.cs_cz,
+			pl = CAKE.pl .. " Izaaka",
 		},
 		[PlayerType.PLAYER_MAGDALENE] = {
 			en_us = "Magdelene's " .. CAKE.en_us,
 			ru = CAKE.ru .. " Магдалины",
 			spa = CAKE.spa .. " de Magdalena",
 			cs_cz = "Magdalénin " .. CAKE.cs_cz,
+			pl = CAKE.pl .. " Magdaleny",
 		},
 		[PlayerType.PLAYER_CAIN] = {
 			en_us = "Cain's " .. CAKE.en_us,
 			ru = CAKE.ru .. " Каина",
 			spa = CAKE.spa .. " de Caín",
 			cs_cz = "Kainův " .. CAKE.cs_cz,
+			pl = CAKE.pl .. " Kaina",
 		},
 		[PlayerType.PLAYER_JUDAS] = {
 			en_us = "Judas '" .. CAKE.en_us,
 			ru = CAKE.ru .. " Иуды",
 			spa = CAKE.spa .. " de Judas",
 			cs_cz = "Jidášův " .. CAKE.cs_cz,
+			pl = CAKE.pl .. " Judasza",
 		},
 		[PlayerType.PLAYER_BLUEBABY] = {
 			en_us = "???'s " .. CAKE.en_us,
 			ru = CAKE.ru .. " ???",
 			spa = CAKE.spa .. " de ???",
 			cs_cz = CAKE.cs_cz .. " ???",
+			pl = CAKE.pl .. " ???",
 		},
 		[PlayerType.PLAYER_EVE] = {
 			en_us = "Eve's " .. CAKE.en_us,
 			ru = CAKE.ru .. " Евы",
 			spa = CAKE.spa .. " de Eva",
 			cs_cz = "Evin " .. CAKE.cs_cz,
+			pl = CAKE.pl .. " Ewy",
 		},
 		[PlayerType.PLAYER_SAMSON] = {
 			en_us = "Samson's " .. CAKE.en_us,
 			ru = CAKE.ru .. " Самсона",
 			spa = CAKE.spa .. " de Sansón",
 			cs_cz = "Samsonův " .. CAKE.cs_cz,
+			pl = CAKE.pl .. " Samsona",
 		},
 		[PlayerType.PLAYER_AZAZEL] = {
 			en_us = "Azazel's " .. CAKE.en_us,
 			ru = CAKE.ru .. " Азазеля",
 			spa = CAKE.spa .. " de Azazel",
 			cs_cz = "Azazelův " .. CAKE.cs_cz,
+			pl = CAKE.pl .. " Azazela",
 		},
 		[PlayerType.PLAYER_LAZARUS] = {
 			en_us = "Lazarus '" .. CAKE.en_us,
 			ru = CAKE.ru .. " Лазаря",
 			spa = CAKE.spa .. " de Lázaro",
 			cs_cz = "Lazarův " .. CAKE.cs_cz,
+			pl = CAKE.pl .. " Łazarza",
 		},
 		[PlayerType.PLAYER_EDEN] = {
 			en_us = "Eden's " .. CAKE.en_us,
 			ru = CAKE.ru .. " Идена",
 			spa = CAKE.spa .. " de Edén",
 			cs_cz = "Edenův " .. CAKE.cs_cz,
+			pl = CAKE.pl .. " Edena",
 		},
 		[PlayerType.PLAYER_THELOST] = {
 			en_us = "The Lost's " .. CAKE.en_us,
@@ -124,42 +135,49 @@ local translations = {
 			ru = CAKE.ru .. " Лилит",
 			spa = CAKE.spa .. " de Lilith",
 			cs_cz = "Lilithin " .. CAKE.cs_cz,
+			pl = CAKE.pl .. " Lilit", --na polskiej wikipedii jest zapisywana jako "Lilith", ale polskie EID zapisuje jako "Lilit"
 		},
 		[PlayerType.PLAYER_KEEPER] = {
 			en_us = "Keeper's " .. CAKE.en_us,
 			ru = CAKE.ru .. " Хранителя",
 			spa = CAKE.spa .. " de Keeper",
 			cs_cz = "Držitelův " .. CAKE.cs_cz,
+			pl = CAKE.pl .. " Dozorcy", --Polskie EID tak to tłumaczy
 		},
 		[PlayerType.PLAYER_APOLLYON] = {
 			en_us = "Apollyon's " .. CAKE.en_us,
 			ru = CAKE.ru .. " Апполиона",
 			spa = CAKE.spa .. " de Apolión",
 			cs_cz = "Apollyónův " .. CAKE.cs_cz,
+			pl = CAKE.pl .. " Apollyona", --EID nie zmienia mu nazwy. Polska wikipedia nazywa go "Abaddon"
 		},
 		[PlayerType.PLAYER_THEFORGOTTEN] = {
 			en_us = "The Forgotten's " .. CAKE.en_us,
 			ru = CAKE.ru .. " Забытого",
 			spa = CAKE.spa .. " de El Olvidado",
 			cs_cz = CAKE.cs_cz .. " Zapomenutého",
+			pl = CAKE.pl .. " Zapomnianego",
 		},
 		[PlayerType.PLAYER_BETHANY] = {
 			en_us = "Bethany's " .. CAKE.en_us,
 			ru = CAKE.ru .. " Вифании", --Бетани
 			spa = CAKE.spa .. " de Bethany",
 			cs_cz = "Betániin " .. CAKE.cs_cz,
+			pl = CAKE.pl .. " Betanii",
 		},
 		[PlayerType.PLAYER_JACOB] = {
 			en_us = "Jacob's " .. CAKE.en_us,
 			ru = CAKE.ru .. " Якова",
 			spa = CAKE.spa .. " de Jacob",
 			cs_cz = "Jákobův " .. CAKE.cs_cz,
+			pl = CAKE.pl .. " Jakuba",
 		},
 		[PlayerType.PLAYER_ESAU] = {
 			en_us = "Esau's " .. CAKE.en_us,
 			ru = CAKE.ru .. " Исава",
 			spa = CAKE.spa .. " de Esaú",
 			cs_cz = "Ezauův " .. CAKE.cs_cz,
+			pl = CAKE.pl .. " Ezawa",
 		},
 	},
 
@@ -169,171 +187,205 @@ local translations = {
 			en_us = "Bonus roll",
 			ru = "Бонусный переброс",
 			cs_cz = "Bonusový hod",
+			pl = "Bonusowe przelosowanie", --szczerze, to brzmi badziewnie, chciałem dać "dodatkowy los" ale za bardzo kojarzy mi się z hazardem a za mało "rerollowaniem"
 		},
 		[PlayerType.PLAYER_MAGDALENE] = {
 			en_us = "Healing power",
 			ru = "Сила исцеления",
 			cs_cz = "Síla léčení",
+			pl = "Siła leczenia",
 		},
 		[PlayerType.PLAYER_CAIN] = {
 			en_us = "Sleight of hand",
 			ru = "Ловкость рук",
 			cs_cz = "Fígl",
+			pl = "Lepkie ręce", --dosłownie tłumaczenie brzmi badziewnie. "Lepkie ręcę" jest używane w języku
 		},
 		[PlayerType.PLAYER_JUDAS] = {
 			en_us = "Sinner's guard",
 			ru = "Страж грешника",
 			cs_cz = "Hříšníkův strážce",
+			pl = "Straż Grzesznika", --ej Monwil, sprawdź wymowę rosyjskiego tłumaczenia lub przeczytaj czeskie :trololed:
 		},
 		[PlayerType.PLAYER_BLUEBABY] = {
 			en_us = "Loose bowels",
 			ru = "Дряблый кишечник",
 			cs_cz = "Řídká stolice",
+			pl = "Erupcja Wezuwiusza", --przepraszam. Nie mogłem się powstrzymać.
 		},
 		[PlayerType.PLAYER_EVE] = {
 			en_us = "",
 			ru = "",
 			cs_cz = "",
+			pl = "",
 		},
 		[PlayerType.PLAYER_SAMSON] = {
 			en_us = "",
 			ru = "",
 			cs_cz = "",
+			pl = "",
 		},
 		[PlayerType.PLAYER_AZAZEL] = {
 			en_us = "Deeper breaths",
 			ru = "Дыши глубже...",
 			cs_cz = "Hluboké dýchání",
+			pl = "Daleki wydech", --szczerze to jest takie pół śmiechem pisane
 		},
 		[PlayerType.PLAYER_LAZARUS] = {
 			en_us = "Come down with me",
 			ru = "Снизойди со мной",
 			cs_cz = "Sejdi se mnou",
+			pl = "Polegnijcie, ze mną", --idk mi się nie podoboa co ty myślisz?
 		},
 		[PlayerType.PLAYER_EDEN] = {
 			en_us = "Variety flavor",
 			ru = "Вкус разнообразия",
 			cs_cz = "Všehochuť",
+			pl = "Smak różnorodności",
 		},
 		[PlayerType.PLAYER_THELOST] = {
 			en_us = "Regained power",
 			ru = "Возвращенная сила",
 			cs_cz = "Obnovená moc",
+			pl = "Odnaleziona siła", --chciałem dać "odnaleziona" bo tłumaczą losta jako "zaginionego". Ale nie wiem :/
 		},
 		[PlayerType.PLAYER_LILITH] = {
 			en_us = "Remember to share!",
 			ru = "Не забудь поделиться!",
 			cs_cz = "Nezapomeň se podělit!",
+			pl = "Pamiętaj, s się dzielić!",
 		},
 		[PlayerType.PLAYER_KEEPER] = {
 			en_us = "Spare change",
 			ru = "Лишняя мелочь",
 			cs_cz = "Drobné",
+			pl = "Drobne",
 		},
 		[PlayerType.PLAYER_APOLLYON] = {
 			en_us = "Snack time!",
 			ru = "Время перекуса!",
 			cs_cz = "Sváča!",
+			pl = "Smakołyki!",
 		},
 		[PlayerType.PLAYER_THEFORGOTTEN] = {
 			en_us = "Revitalize",
 			ru = "Оживление",
 			cs_cz = "Oživení",
+			pl = "Ożywiony",
 		},
 		[PlayerType.PLAYER_BETHANY] = {
 			en_us = "Harmony of body and soul",
 			ru = "Гармония души и тела",
 			cs_cz = "Rovnováha těla a duše",
+			pl = "Harmonia ciała i duszy",
 		},
 		[PlayerType.PLAYER_JACOB] = {
 			en_us = "Stronger than you",
 			ru = "Сильнее, чем ты",
 			cs_cz = "Silnější než ty",
+			pl = "Silniejszy od ciebie",
 		},
 		[PlayerType.PLAYER_ISAAC_B] = {
 			en_us = "Pocket item",
 			ru = "Карманный предмет",
 			cs_cz = "Kapesní předmět",
+			pl = "Podręczny przedmiot",
 		},
 		[PlayerType.PLAYER_MAGDALENE_B] = {
 			en_us = "Heart attack",
 			ru = "Сердечный приступ",
 			cs_cz = "Infarkt",
+			pl = "Zawał",
 		},
 		[PlayerType.PLAYER_CAIN_B] = {
 			en_us = "Repurpose",
 			ru = "Перераспределение",
 			cs_cz = "Opětovné použití",
+			pl = "Recykling",
 		},
 		[PlayerType.PLAYER_JUDAS_B] = {
 			en_us = "",
 			ru = "",
 			cs_cz = "",
+			pl = "",
 		},
 		[PlayerType.PLAYER_BLUEBABY_B] = {
 			en_us = "Sturdy turdy",
 			ru = "Твердый кал",
 			cs_cz = "Tvrdé hovno",
+			pl = "Chlupie w dupie", --wymyśl coś lepszego, teraz mnie to bawi ale za 5 miesięcy będę z tego cringe'ował.
 		},
 		[PlayerType.PLAYER_EVE_B] = {
 			en_us = "",
 			ru = "",
 			cs_cz = "",
+			pl = "",
 		},
 		[PlayerType.PLAYER_SAMSON_B] = {
 			en_us = "Unending rampage",
 			ru = "Нескончаемая ярость",
 			cs_cz = "Nekonečné běsnění",
+			pl = "Nieskończony gniew",
 		},
 		[PlayerType.PLAYER_AZAZEL_B] = {
 			en_us = "Allergy up",
 			ru = "Аллергия ↑",
 			cs_cz = "Alergie ↑",
+			pl = "Alergie ↑",
 		},
 		[PlayerType.PLAYER_LAZARUS_B] = {
 			en_us = "A gift from beyond",
 			ru = "Дар с небес",
 			cs_cz = "Dar z neznáma",
+			pl = "Dar z nie tego świata"
 		},
 		[PlayerType.PLAYER_EDEN_B] = {
 			en_us = "",
 			ru = "",
 			cs_cz = "",
+			pl = "",
 		},
 		[PlayerType.PLAYER_THELOST_B] = {
 			en_us = "",
 			ru = "",
 			cs_cz = "",
+			pl = "",
 		},
 		[PlayerType.PLAYER_LILITH_B] = {
 			en_us = "",
 			ru = "",
 			cs_cz = "",
+			pl = "",
 		},
 		[PlayerType.PLAYER_KEEPER_B] = {
 			en_us = "Local business",
 			ru = "Местный бизнес",
 			cs_cz = "Místní podnik",
+			pl = "Lokalny biznes",
 		},
 		[PlayerType.PLAYER_APOLLYON_B] = {
 			en_us = "Harvest",
 			ru = "Сбор урожая",
 			cs_cz = "Sklizeň",
+			pl = "Żniwa",
 		},
 		[PlayerType.PLAYER_THEFORGOTTEN_B] = {
 			en_us = "Fracture",
 			ru = "Перелом",
 			cs_cz = "Zlomenina",
+			pl = "Rozpad",
 		},
 		[PlayerType.PLAYER_BETHANY_B] = {
 			en_us = "Stronger summons",
 			ru = "Более сильные огоньки", --"Призывы" don't sound very good
 			cs_cz = "Silnější vyvolání",
+			pl = "Silniejsze ogniki",
 		},
 		[PlayerType.PLAYER_JACOB_B] = {
 			en_us = "",
 			ru = "",
 			cs_cz = "",
+			pl = "",
 		},
 	},
 
