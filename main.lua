f86c84f65cf6e693ae6125f09a8b5d0566d7b107
BenevolusGoat <brude602@gmail.com>
2025-01-17 17:02:54 -0500

    J&E have separate sprites, added legacy and blurb support, finished HUDHelper Trinket rendering

--- main.lua
+++ main.lua
@@ -145,10 +145,10 @@ BirthcakeRebaked.BirthcakeSprite = {
 	},
 }
 BirthcakeRebaked.ModCallbacks = {
-	---(player: EntityPlayer): string, Optional Arg: PlayerType - Called when getting the player-specific name of Birthcake before displayed as item text. Return a string to override it
-	PRE_BIRTHCAKE_ITEMTEXT_NAME = "BIRTHCAKE_PRE_BIRTHCAKE_ITEMTEXT_NAME",
-	---(player: EntityPlayer): string, Optional Arg: PlayerType - Called when getting the player-specific description of Birthcake before displayed as item text. Return a string to override it
-	PRE_BIRTHCAKE_ITEMTEXT_DESCRIPTION = "BIRTHCAKE_PRE_BIRTHCAKE_ITEMTEXT_DESCRIPTION",
+	---(player: EntityPlayer, name: string): string, Optional Arg: PlayerType - Called when getting the player-specific name of Birthcake before displayed as item text. Return a string to override it
+	GET_BIRTHCAKE_ITEMTEXT_NAME = "BIRTHCAKE_GET_BIRTHCAKE_ITEMTEXT_NAME",
+	---(player: EntityPlayer, description: string): string, Optional Arg: PlayerType - Called when getting the player-specific description of Birthcake before displayed as item text. Return a string to override it
+	GET_BIRTHCAKE_ITEMTEXT_DESCRIPTION = "BIRTHCAKE_GET_BIRTHCAKE_ITEMTEXT_DESCRIPTION",
 	---(player: EntityPlayer, sprite: Sprite), Optional Arg: Player Type - Called when loading the player-specific Birthcake sprite. Return a sprite object to override it
 	LOAD_BIRTHCAKE_SPRITE = "BIRTHCAKE_LOAD_BIRTHCAKE_SPRITE",
 	PRE_BIRTHCAKE_RENDER = "BIRTHCAKE_PRE_BIRTHCAKE_RENDER",
@@ -158,225 +158,90 @@ BirthcakeRebaked.ModCallbacks = {
 	---(player: EntityPlayer, firstTimePickup: boolean, isGolden: boolean), Optional Arg: PlayerType - Called when collecting the Birthcake trinket. REPENTOGON will trigger this any time the item is added to the player's inventory. Otherwise, it triggers only when picked up as a trinket
 	POST_BIRTHCAKE_COLLECT = "BIRTHCAKE_POST_BIRTHCAKE_COLLECT"
 }
+local translations = include("src_birthcake.birthcake_translations")
+BirthcakeRebaked.BirthcakeNames = translations.BIRTHCAKE_NAME
+BirthcakeRebaked.BirthcakeDescriptions = translations.BIRTHCAKE_DESCRIPTION
+BirthcakeRebaked.EID = translations.EID
 
---[[ Mod.TrinketDesc = {
-	----- Isaac -----
-	[0] =
-	{
-		["Normal"] = "When rerolling, items have a 25% chance to cycle back and forth between their new and original form";
-	};
-	[21] =
-	{
-		["Normal"] = "Getting hit while having no empty item slot has a 25% chance to permanently exclude a random item from occupying an invetory slot #The trinket has a 50% chance to disappear if this occurs";
-	};
-	----- Magdalene -----
-	[1] =
-	{
-		["Normal"] = "Higher chance for red hearts to be replaced by double red hearts # {{HalfSoulHeart}} Yum heart refills half soul heart upon use";
-		["Upgraded"] = "Higher chance for red hearts to be replaced by double red hearts # {{SoulHeart}} Yum heart refills {{ColorGold}}one {{ColorText}}soul heart upon use"
-	};
-	[22] =
-	{
-		["Normal"] = "Uncollected red hearts dropped from enemies create small blood explosions when they disappear, damaging nearby enemies";
-	};
-	----- Cain -----
-	[2] =
-	{
-		["Normal"] = "↑ +1 Luck# Interacting with machines has a 33% chance to not consume coins# The chance is reduced to 25% for the crane game";
-		["Upgraded"] = "↑ +1 Luck# Interacting with machines has a {{ColorGold}}50{{ColorText}}% chance to not consume coins# The chance is reduced to {{ColorGold}}33{{ColorText}}% for the crane game";
-	};
-	[23] =
-	{
-		["Normal"] = "Double pickups spawn as two individual pickups instead #Putting a pickup in the bag has a 5% chance to add it to your inventory";
-	};
-	----- Judas -----
-	[3] =
-	{
-		["Normal"] = "↑ +{{DamageUp}} damage #Grants +1 black heart when selling all of your red health to a devil deal";
-		["Upgraded"] = "↑ +{{ColorGold}}{{DamageUp}}{{ColorText}} damage #Grants +1 black heart when selling all of your red health to a devil deal"
-	};
-	[24] =
-	{
-		["Normal"] = "Enemies marked by dark arts are slowed down after the attack is executed";
-	};
-	----- Blue Baby -----
-	[4] =
-	{
-		["Normal"] = "Active items spawn number of active item charges - 1 poops upon use # Each poop has a 10% chance to be replaced by tainted ???'s corn poop";
-		["Mom's Box"] = "Active items spawn number of active item charges - 1 poops upon use # Each poop has a {{ColorGold}}20{{ColorText}}% chance to be replaced by tainted ???'s corn poop";
-		["+Golden"] = "# {{ColorGold}}50% chance for spawned poop to be golden {{ColorText}}";
-		["+{{Trinket"..TrinketType.TRINKET_MECONIUM.."}}"] = "# {{Trinket".. TrinketType.TRINKET_MECONIUM .."}} {{ColorGray}}Poops spawned have a chance to be black poop"
-	};
-	[25] =
-	{
-		["Normal"] = "Thrown poops are resistant to enemy projectiles #Enemies can go through poops, but are heavily slowed down when doing so";
-	};
-	----- Eve -----
-	[5] =
-	{
-		["Normal"] = "Prioritizes Eve's red health when taking damage #Losing red health doesn't affect devil/angel deal chances if eve has at least one soul heart";
-		["Upgraded"] = "Prioritizes Eve's red health when taking damage #Losing red health doesn't affect devil/angel deal chances if eve has at least one soul heart # {{ColorGold}}Also replace bone hearts behind soul hearts when taking damage";
-	};
-	[26] =
-	{
-		["Normal"] = "Spawns 2 clots at a time at the cost of draining twice as much health # Clots leave damaging creep upon death";
-	};
-	----- Samson -----
-	[6] =
-	{
-		["Normal"] = "Drops 2 red hearts when full rage is reached";
-		["Upgraded"] = "Drop {{ColorGold}}2 hearts based on on your current health{{ColorText}} when full rage is reached";
-		["+Birthright"] = "# {{Collectible".. CollectibleType.COLLECTIBLE_BIRTHRIGHT .."}} {{ColorGray}}Also drop 2 hearts when 6 rage is reached";
-	};
-	[27] =
-	{
-		["Normal"] = "While in berserk mode, has a chance to spawn a random hearts and extends berserk mode for 5 seconds";
-	};
-	----- Azazel -----
-	[7] =
-	{
-		["Normal"] = "Holding a fire button while brimstone is fully charged allows azazel to shoot regular tears # Azazel's tears only deal 50% damage but have twice the range";
-	};
-	[28] =
-	{
-		["Normal"] = "When Azazel sneeze on enemies they get poisoned #Azazel also receives knockback when sneezing";
-	};
-	---- Lazarus -----
-	[8] =
-	{
-		["Normal"] = "Upon revival, deals 40 damage to all enemies in the room # When Lazarus Risen revert back to Lazarus, he also get back his lost heart container";
-		["Upgraded"] = "Upon revival, deals {{ColorGold}}{{Chance40}}{{ColorText}} damage to all enemies in the room # When Lazarus Risen revert back to Lazarus, he also get back his lost heart container";
-	};
-	[11] =
-	{
-		["Normal"] = "Upon revival, deals 40 damage to all enemies in the room # When Lazarus Risen revert back to Lazarus, he also get back his lost heart container";
-		["Upgraded"] = "Upon revival, deals {{ColorGold}}{{Chance40}}{{ColorText}} damage to all enemies in the room # When Lazarus Risen revert back to Lazarus, he also get back his lost heart container";
-	};
-	[29] =
-	{
-		["Normal"] = "Upon entering a new room, the form holding the trinket gains a random item from the other form for the duration of the room";
-		["Upgraded"] = "Upon entering a new room, the form holding the trinket gains a random item from the other form for the duration of the room # {{ColorGold}}Prevents Quality {{Quality0}} items from being shared {{ColorText}}"
-	};
-	[38] =
-	{
-		["Normal"] = "Upon entering a new room, the form holding the trinket gains a random item from the other form for the duration of the room";
-		["Upgraded"] = "Upon entering a new room, the form holding the trinket gains a random item from the other form for the duration of the room # {{ColorGold}}Prevents Quality {{Quality0}} items from being shared {{ColorText}}"
-	};
-	----- Eden -----
-	[9] =
-	{
-		["Normal"] = "";
-	};
-	[30] =
-	{
-		["Normal"] = "Pedestal items have a chance to either become glitched or turn into items from different item pools";
-	};
-	----- The lost -----
-	[10] =
-	{
-		["Normal"] = "While holding the Eternal D6, grants 2 guaranteed rerolls per floor that don't require any charges # If the lost is not holding the Eternal D6, he instead gains one free use of his active item per floor";
-	};
-	[31] =
-	{
-		["Normal"] = "Has a 50% chance to spawn an extra item when encountering an item pedestal # If this occurs, the trinket has a 25% chance to self-destrcut, dealing 100 damage to all enemies in the room and removing your mantle in the process";
-	};
-	---- Lillith ----
-	[13] =
-	{
-		["Normal"] = "Familiars have a 25% chance to copy lillith's tear effects # At the start of each floor has a 10% chance to consume the trinket and spawn a demon familiar # If it happens the cake has 25% chance to replace any future trinket";
-	};
-	[32] =
-	{
-		["Normal"] = "Spawn Incubus every other room and Succubus the room between that one # When entering a boss fight, spawn both Incubus and Succubus"; -- KAULIANPOWER
-	};
-	----- Keeper -----
-	[14] =
-	{
-		["Normal"] = "Grants a nickel when entering a shop, a black market or a devil room";
-		["Upgraded"] = "Grants a {{ColorGold}}golden penny{{ColorText}} when entering a shop, a black market or a devil room"
-	};
-	[33] =
-	{
-		["Normal"] = "Spawns an item and 2 pickups for sale in the starting room of each floor";
-	};
-	---- Apollyon -----
-	[15] =
-	{
-		["Normal"] = "Allows void to consume trinkets on the ground # While holding void, you gain the effects of all consumed trinkets";
-	};
-	[34] =
-	{
-		["Normal"] = "Allows abyss to consume trinkets on the ground. Turning them into smaller locusts that deal 50% damage # You don't gain the effect of trinkets consumed by abyss";
-	};
-	---- The Forgotten ----
-	[16] =
-	{
-		["Normal"] = "Upon striking an enemy with the bone club, the forgotten gains an intangible wisp # Switching to the soul turns every wisp into a weak purgatory ghost that grants a temporary dmg boost to the soul";
-	};
-	[17] =
-	{
-		["Normal"] = "Upon striking an enemy with the bone club, the forgotten gains an intangible wisp # Switching to the soul turns every wisp into a weak purgatory ghost that grants a temporary dmg boost to the soul";
-	};
-	[35] =
-	{
-		["Normal"] = "Spawn 3 fly orbital, 2 on the soul and 1 on the bone pile # Enemies are fixated into attacking the bone pile when not near the soul";
-	};
-	[40] =
-	{
-		["Normal"] = "Spawn 3 fly orbital, 2 on the soul and 1 on the bone pile # Enemies are fixated into attacking the bone pile when not near the soul";
-	};
-	---- Bethany -----
-	[18] =
-	{
-		["Normal"] = "Has 10% chance to spawn a basic wisp upon room clear";
-		["Upgraded"] = "Has {{ColorGold}}{{Chance10}}{{ColorText}}% chance to spawn a basic wisp upon room clear"
-	};
-	[36] =
-	{
-		["Normal"] = "The next use of lemegeton will consume this trinket and spawn 2 lemegeton wisps that draw from the current room's item pool";
-		["Golden"] = "{{ColorGold}}Allow lemegeton to spawn 2 lemegeton wisps that draw from the current room's item pool # {{ColorText}}Chance to turn into a regular {{Trinket"..Mod.Birthcake.ID.."}}Birthcake after this occurs";
-	};
-	---- Jacob & Esau -----
-	[19] =
-	{
-		["Normal"] = "The holder gains +25% of the stat the other brother has";
-	};
-	[20] =
-	{
-		["Normal"] = "The holder gains +25% of the stat the other brother has";
-	};
-	[37] =
-	{
-		["Normal"] = "Dark Esau consumes the cake upon damaging Jacob, preventing him from turning into The Lost # After this occurs, Dark Esau and Anima Sola will target enemies for the rest of the floor";
-		["Golden"] = "{{ColorGold}}Allow Jacob to get a free hit from Dark Esau, preventing him from turning into The Lost # {{ColorText}}After this occurs, Dark Esau and Anima Sola will target enemies for the rest of the floor #Chance to turn into a regular {{Trinket"..Mod.Birthcake.ID.."}}Birthcake after being hit";
-	};
-	[39] =
-	{
-		["Normal"] = "Dark Esau consumes the cake upon damaging Jacob, preventing him from turning into The Lost # After this occurs, Dark Esau and Anima Sola will target enemies for the rest of the floor";
-		["Golden"] = "{{ColorGold}}Allow Jacob to get a free hit from Dark Esau, preventing him from turning into The Lost # {{ColorText}}After this occurs, Dark Esau and Anima Sola will target enemies for the rest of the floor #Chance to turn into a regular {{Trinket"..Mod.Birthcake.ID.."}}Birthcake after being hit";
-	};
-} ]]
+local utility = {
+	"hud_helper",
+	"rgon_enums",
+	"misc_util",
+	"birthcake_util"
+}
+for _, path in ipairs(utility) do
+	include("src_birthcake.utility." .. path)
+end
 
+local miscSrc = {
+	"compatibility.birthcake_legacy",
+	"compatibility.patches_loader",
+	"trinket_birthcake",
+	"challenge_birthday_party",
+}
+for _, path in ipairs(miscSrc) do
+	include("src_birthcake." .. path)
+end
+
+local languageOptionToEID = {
+	["en"] = "en_us",
+	["jp"] = "ja_jp",
+	["es"] = "spa",
+	["de"] = "de",
+	["fr"] = "fr",
+	["ru"] = "ru",
+	["kr"] = "ko_kr",
+	["zh"] = "zh_cn"
+}
+
+function BirthcakeRebaked:TryGetTranslation(table)
+	return table[languageOptionToEID[Options.Language]] or table.en_us
+end
+
+---@param player EntityPlayer
+function BirthcakeRebaked:GetBirthcakeName(player)
+	local playerType = player:GetPlayerType()
+	local name = player:GetName() .. "'s Cake"
+	if Mod.BirthcakeNames[playerType] and Mod:TryGetTranslation(Mod.BirthcakeNames[playerType]) then
+		name = Mod:TryGetTranslation(Mod.BirthcakeNames[playerType])
+	end
+	local nameResult = Isaac.RunCallbackWithParam(Mod.ModCallbacks.GET_BIRTHCAKE_ITEMTEXT_NAME, playerType, player, name)
+	name = (nameResult ~= nil and tostring(nameResult)) or name
+	return name
+end
+
+---@param player EntityPlayer
+function BirthcakeRebaked:GetBirthcakeDescription(player)
+	local playerType = player:GetPlayerType()
+	local description = "No effect!" --Placeholder
+	if Mod.BirthcakeDescriptions[playerType] and Mod:TryGetTranslation(Mod.BirthcakeDescriptions[playerType]) then
+		description = Mod:TryGetTranslation(Mod.BirthcakeDescriptions[playerType])
+	elseif Birthcake.BirthcakeDescs[playerType] then
+		description = Birthcake.BirthcakeDescs[playerType]
+	end
+	local descriptionResult = Isaac.RunCallbackWithParam(Mod.ModCallbacks.GET_BIRTHCAKE_ITEMTEXT_DESCRIPTION, playerType,
+		player, description)
+	description = (descriptionResult ~= nil and tostring(descriptionResult)) or description
+	return description
+end
+
+---@param player EntityPlayer
 function BirthcakeRebaked:GetBirthcakeSprite(player)
 	local playerType = player:GetPlayerType()
-	local config = Mod.BirthcakeSprite[playerType]
+	local spriteConfig = Mod.BirthcakeSprite[playerType]
 	local sprite = Sprite()
 	sprite:Load("gfx/005.350_trinket.anm2", false)
-	sprite:ReplaceSpritesheet(0, config.SpritePath)
-	sprite:LoadGraphics()
+	if spriteConfig then
+		sprite:ReplaceSpritesheet(0, spriteConfig.SpritePath)
+		sprite:LoadGraphics()
+	elseif not spriteConfig and Birthcake.BirthcakeDescs[playerType] then
+		sprite:ReplaceSpritesheet(0, trinketPath .. player:GetName():lower() .. "_birthcake.png")
+	end
 	local spriteResult = Isaac.RunCallbackWithParam(Mod.ModCallbacks.LOAD_BIRTHCAKE_SPRITE, playerType, player, sprite)
 	sprite = (spriteResult ~= nil and type(spriteResult) == "userdata" and getmetatable(spriteResult).__type == "Sprite" and spriteResult) or
 		sprite
 	return sprite
 end
-
-include("src_birthcake.utility.hud_helper")
-include("src_birthcake.utility.rgon_enums")
-include("src_birthcake.utility.misc_util")
-include("src_birthcake.utility.birthcake_util")
---include("src_birthcake.mcm")
-include("src_birthcake.trinket_birthcake")
-include("src_birthcake.challenge_birthday_party")
 
 ---@param player EntityPlayer
 function BirthcakeRebaked:ItemDesc(player)
@@ -391,15 +256,10 @@ function BirthcakeRebaked:ItemDesc(player)
 	then
 		data.HoldingBirthcake = queuedItem.ID
 		data.BirthcakeFirstPickup = player.QueuedItem.Touched
-		local name = player:GetName() .. "'s Cake"
-		local description = "Placeholder till I put in translations lol"
-		local nameResult = Isaac.RunCallbackWithParam(Mod.ModCallbacks.PRE_BIRTHCAKE_ITEMTEXT_NAME, playerType, player)
-		local descriptionResult = Isaac.RunCallbackWithParam(Mod.ModCallbacks.PRE_BIRTHCAKE_ITEMTEXT_DESCRIPTION,
-			playerType,
-			player)
-		name = (nameResult ~= nil and tostring(nameResult)) or name
-		description = (descriptionResult ~= nil and tostring(descriptionResult)) or description
-		Mod.Game:GetHUD():ShowItemText(name, description, false)
+		local name = Mod:GetBirthcakeName(player)
+		local description = Mod:GetBirthcakeDescription(player)
+
+		Mod.Game:GetHUD():ShowItemText(name, description, false, true)
 
 		local sprite = BirthcakeRebaked:GetBirthcakeSprite(player)
 		sprite:Play("PlayerPickupSparkle")
@@ -436,14 +296,17 @@ function BirthcakeRebaked:ChangeSpritePickup(pickup)
 
 	if Mod:IsBirthcake(pickup.SubType) then
 		local sprite = pickup:GetSprite()
-		local config = Mod.BirthcakeSprite[playerType]
+		local spriteConfig = Mod.BirthcakeSprite[playerType]
 		local anim = sprite:GetAnimation()
-		if config.Anm2 then
-			sprite:Load(config.Anm2, true)
+		if spriteConfig and spriteConfig.Anm2 then
+			sprite:Load(spriteConfig.Anm2, true)
 			sprite:Play(anim)
 		end
-		if config.SpritePath then
-			sprite:ReplaceSpritesheet(0, config.SpritePath)
+		if spriteConfig and spriteConfig.SpritePath then
+			sprite:ReplaceSpritesheet(0, spriteConfig.SpritePath)
+		end
+		if not spriteConfig and Birthcake.BirthcakeDescs[playerType] then
+			sprite:ReplaceSpritesheet(0, trinketPath .. player:GetName():lower() .. "_birthcake.png")
 		end
 		sprite:LoadGraphics()
 
