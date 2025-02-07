4d70de36bffc413bf5c02d6a2cd390fb639d4f6d
BenevolusGoat <brude602@gmail.com>
2025-02-07 09:21:40 -0500

    Forgot to update this lol

--- src_birthcake/compatibility/patches/epiphany.lua
+++ src_birthcake/compatibility/patches/epiphany.lua
@@ -2,7 +2,7 @@ local Mod = BirthcakeRebaked
 local loader = Mod.PatchesLoader
 
 local function epiphanyPatch()
-	Mod.API:AddBirthcakePickupText(Epiphany.technical_character, "Coming soon?", "Technical's")
+	Mod.API:AddBirthcakePickupText(Epiphany.technical_character, "It's all a mystery!", "Technical's")
 	Mod.API:AddBirthcakeSprite(Epiphany.technical_character, {
 		SpritePath = "gfx/items/trinkets/technical_character_birthcake.png"
 	})
